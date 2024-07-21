ddd708a8f864d129dc76df6baf1568b28d63cf96
vinay_sharma <vinaysharmaxyzo@gmail.com>
2024-07-21 09:23:48 +0530

    how to update tuple

---
+++ tuple/update.py
@@ -0,0 +1,22 @@
+
+# tuple_1 = ("apple","banana","pineapple")
+# # len function count the 
+# print(len(tuple_1))
+
+
+# b = ("vinay",)
+# c = type(b)
+
+# print(c)
+
+
+tuple_1 = ("apple","banana","pineapple")
+b = list(tuple_1)
+print(type(b))
+b[0]="vinay"
+c = tuple(b)
+print(c)
+print(type(c))
+
+
+
